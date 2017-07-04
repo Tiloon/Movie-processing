0e7f5556a1b2a88f7f4fe450d5f8c64618ed6eff
Hugo Rybinski <rybins_h@epita.fr>
2017-07-04 16:07:05 +0200

    add setup for notebook

---
+++ sparkNotebook/setup.sh
@@ -0,0 +1,2 @@
+sudo docker pull andypetrella/spark-notebook:0.7.0-scala-2.11.8-spark-2.1.1-hadoop-2.7.2
+sudo docker run -p 9001:9001  andypetrella/spark-notebook:0.7.0-scala-2.11.8-spark-2.1.1-hadoop-2.7.2
