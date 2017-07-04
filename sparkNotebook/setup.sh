b4d367169ae646048e34d10d425447a0db4454b8
Hugo Rybinski <rybins_h@epita.fr>
2017-07-04 16:56:18 +0200

    add usefull command for notebook docker

--- sparkNotebook/setup.sh
+++ sparkNotebook/setup.sh
@@ -1,2 +1,4 @@
 sudo docker pull andypetrella/spark-notebook:0.7.0-scala-2.11.8-spark-2.1.1-hadoop-2.7.2
-sudo docker run -p 9001:9001  andypetrella/spark-notebook:0.7.0-scala-2.11.8-spark-2.1.1-hadoop-2.7.2
+sudo docker run --name='notebookSpark' -p 9001:9001  andypetrella/spark-notebook:0.7.0-scala-2.11.8-spark-2.1.1-hadoop-2.7.2
+sudo docker stop notebookSpark
+sudo docker start notebookSpark
