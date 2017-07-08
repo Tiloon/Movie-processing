c1ecad4d983110bd6464010c09902a0ef6f07a67
Hugo Rybinski <rybins_h@epita.fr>
2017-07-09 00:05:17 +0200

    add script for pickle

--- analyzeData/script.py
+++ analyzeData/script.py
@@ -1,17 +1,30 @@
 #!/usr/bin/python3
 import sys
 import json
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import LinearSVC
+import pickle
 
-def handleLine(line):
+
+class Payload:
+    def __init__(self, svm, vect):
+        self.svm = svm
+        self.vect = vect
+
+    def use(self, comm):
+        return self.svm.predict(self.vect.transform(comm))
+
+def handleLine(line, payload):
     if line:
         ans = json.loads(line)
         comments = ans['comments']
-        nbWord = 0
         for comment in comments:
-            nbWord += len(comment['content'].split())
-        ans['nbWords'] = nbWord
+            comment['sentiment'] = 1 if payload.use([comment['content'].encode()])[0] == 'positive' else 0
         print(ans)
 
 
-for line in sys.stdin:
-    handleLine(line.strip(" \n\r"))
+with open(sys.argv[0].rsplit('/', 1)[0] + '/payload.pickle', 'rb') as f:
+    payload = pickle.load(f)
+
+    for line in sys.stdin:
+        handleLine(line.strip(" \n\r"), payload)
